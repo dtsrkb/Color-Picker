bfde58010e628972923c291c4200913fddc63ebe
Drew Smith <drewsmith@mwc-053230.dhcp.missouri.edu>
2018-04-16 13:41:20 -0500

    First implementation of requirements

---
+++ Color Picker/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  Color Picker
+//
+//  Created by Drew Smith on 4/16/18.
+//  Copyright © 2018 Tech Innovator. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
--- Color Picker/ViewController.swift
+++ Color Picker/ViewController.swift
@@ -8,10 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    //let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+    
+    let colors = [Color(name: "Red", uiColor: UIColor.red),
+                  Color(name: "Orange", uiColor: UIColor.orange),
+                  Color(name: "Yellow", uiColor: UIColor.yellow),
+                  Color(name: "Green", uiColor: UIColor.green),
+                  Color(name: "Blue", uiColor: UIColor.blue),
+                  Color(name: "Purple", uiColor: UIColor.purple)]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row].name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        colorLabel.text = colors[row].name
+        self.view.backgroundColor = colors[row].uiColor
+    }
+    
+    
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    @IBOutlet weak var colorPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        colorPickerView.selectRow(0, inComponent: 0, animated: true)
+        colorLabel.text = colors[colorPickerView.selectedRow(inComponent: 0)].name
+        self.view.backgroundColor = colors[colorPickerView.selectedRow(inComponent: 0)].uiColor
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
